d52a253acf3c5fc0e1961c23f497946cbd8ef2b8
ChenYu-Liu <4a1g0007@stust.edu.tw>
2017-09-19 17:25:31 +0800

    modify utSort.h

--- utSort.h
+++ utSort.h
@@ -8,19 +8,31 @@
 #include<math.h>
 using namespace std;
 
+vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
+Rectangle rectangle(2., 3., 4., 5., "rectangle");
+Circle circle(2, 3, 4, "circle");
+Triangle triangle(one, two, three, "triangle");
+//Shape 容器
+list<Shape*> ShapeList{ &rectangle, &circle, &triangle };
+//ShapeList pointer
+list<Shape*>::iterator it;
+//lambda experssion (指標的部分，事後要再弄清楚↓)
+auto ByIncreasPerimeter = [&](const Shape* a, const Shape* b)->bool{if ((*a).perimeter() < (*b).perimeter())return true; else return false; };
+auto ByDecreasingPerimeter = [&](const Shape* a, const Shape* b)->bool{if ((*a).perimeter() > (*b).perimeter())return true; else return false; };
+auto ByIncreasAera = [&](const Shape* a, const Shape* b)->bool{if ((*a).area() < (*b).area())return true; else return false; };
+auto ByDecreasingAera = [&](const Shape* a, const Shape* b)->bool{if ((*a).area() > (*b).area())return true; else return false; };
+auto ByIncreasCompactness = [&](const Shape* a, const Shape* b)->bool{
+	double first = 4 * M_PI*(*a).area() / pow((*a).perimeter(), 2);
+	double second = 4 * M_PI*(*b).area() / pow((*b).perimeter(), 2);
+	if (first < second)
+		return true;
+	else
+		return false;
+};
+
 TEST(Sort, sortByIncreasingPerimeter) {
-	list<Shape*> ShapeList;
-	vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
-	//指標的部分，事後要再弄清楚↓
-	auto Compare = [&](const Shape* a, const Shape* b)->bool{if ((*a).perimeter() < (*b).perimeter())return true; else return false; };
-	Rectangle rectangle(2., 3., 4., 5., "rectangle");
-	Circle circle(2, 3, 4, "circle");
-	Triangle triangle(one, two, three, "triangle");
-	ShapeList.push_back(&rectangle);
-	ShapeList.push_back(&circle);
-	ShapeList.push_back(&triangle);
-	ShapeList.sort(Compare);
-	list<Shape*>::iterator it = ShapeList.begin();
+	ShapeList.sort(ByIncreasPerimeter);
+	it = ShapeList.begin();
 	ASSERT_EQ("triangle(2 2 3.1 2.3 1.2 3.2)", (*it)->toString());
 	it++;
 	ASSERT_EQ("rectangle(2 3 4 5)", (*it)->toString());
@@ -29,18 +41,8 @@ TEST(Sort, sortByIncreasingPerimeter) {
 }
 
 TEST(Sort, sortByDecreasingPerimeter) {
-	list<Shape*> ShapeList;
-	vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
-	//指標的部分，事後要再弄清楚↓
-	auto Compare = [&](const Shape* a, const Shape* b)->bool{if ((*a).perimeter() > (*b).perimeter())return true; else return false; };
-	Rectangle rectangle(2., 3., 4., 5., "rectangle");
-	Circle circle(2, 3, 4, "circle");
-	Triangle triangle(one, two, three, "triangle");
-	ShapeList.push_back(&rectangle);
-	ShapeList.push_back(&circle);
-	ShapeList.push_back(&triangle);
-	ShapeList.sort(Compare);
-	list<Shape*>::iterator it = ShapeList.begin();
+	ShapeList.sort(ByDecreasingPerimeter);
+	it = ShapeList.begin();
 	ASSERT_EQ("circle(2 3 4)", (*it)->toString());
 	it++;
 	ASSERT_EQ("rectangle(2 3 4 5)", (*it)->toString());
@@ -49,18 +51,8 @@ TEST(Sort, sortByDecreasingPerimeter) {
 }
 
 TEST(Sort, sortByIncreasingArea) {
-	list<Shape*> ShapeList;
-	vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
-	//指標的部分，事後要再弄清楚↓
-	auto Compare = [&](const Shape* a, const Shape* b)->bool{if ((*a).area() < (*b).area())return true; else return false; };
-	Rectangle rectangle(2., 3., 4., 5., "rectangle");
-	Circle circle(2, 3, 4, "circle");
-	Triangle triangle(one, two, three, "triangle");
-	ShapeList.push_back(&rectangle);
-	ShapeList.push_back(&circle);
-	ShapeList.push_back(&triangle);
-	ShapeList.sort(Compare);
-	list<Shape*>::iterator it = ShapeList.begin();
+	ShapeList.sort(ByIncreasAera);
+	it = ShapeList.begin();
 	ASSERT_EQ("triangle(2 2 3.1 2.3 1.2 3.2)", (*it)->toString());
 	it++;
 	ASSERT_EQ("rectangle(2 3 4 5)", (*it)->toString());
@@ -69,47 +61,19 @@ TEST(Sort, sortByIncreasingArea) {
 }
 
 TEST(Sort, sortByDecreasingArea) {
-	list<Shape*> ShapeList;
-	vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
-	//指標的部分，事後要再弄清楚↓
-	auto Compare = [&](const Shape* a, const Shape* b)->bool{if ((*a).area() > (*b).area())return true; else return false; };
-	Rectangle rectangle(2., 3., 4., 5., "rectangle");
-	Circle circle(2, 3, 4, "circle");
-	Triangle triangle(one, two, three, "triangle");
-	ShapeList.push_back(&rectangle);
-	ShapeList.push_back(&circle);
-	ShapeList.push_back(&triangle);
-	ShapeList.sort(Compare);
-	list<Shape*>::iterator it = ShapeList.begin();
+	ShapeList.sort(ByDecreasingAera);
+	it = ShapeList.begin();
 	ASSERT_EQ("circle(2 3 4)", (*it)->toString());
 	it++;
 	ASSERT_EQ("rectangle(2 3 4 5)", (*it)->toString());
 	it++;
 	ASSERT_EQ("triangle(2 2 3.1 2.3 1.2 3.2)", (*it)->toString());
 
-
 }
 
 TEST(Sort, sortByIncreasingCompactness) {
-	list<Shape*> ShapeList;
-	vertex one = { 2.0f, 2.0f }; vertex two = { 3.1f, 2.3f }; vertex three = { 1.2f, 3.2f };
-	//4 * PI * aera / pow(perimeter,2) Or "Gibbs" 4 * aera /(pi * pow(length,2))
-	auto Compare = [&](const Shape* a, const Shape* b)->bool{
-		double first = 4 * M_PI*(*a).area() / pow((*a).perimeter(), 2);
-		double second = 4 * M_PI*(*b).area() / pow((*b).perimeter(), 2);
-		if (first < second)
-			return true; 
-		else
-			return false;
-	};
-	Rectangle rectangle(2., 3., 4., 5., "rectangle");
-	Circle circle(2, 3, 4, "circle");
-	Triangle triangle(one, two, three, "triangle");
-	ShapeList.push_back(&rectangle);
-	ShapeList.push_back(&circle);
-	ShapeList.push_back(&triangle);
-	ShapeList.sort(Compare);
-	list<Shape*>::iterator it = ShapeList.begin();
+	ShapeList.sort(ByIncreasCompactness);
+	it = ShapeList.begin();
 	ASSERT_EQ("triangle(2 2 3.1 2.3 1.2 3.2)", (*it)->toString());
 	it++;
 	ASSERT_EQ("rectangle(2 3 4 5)", (*it)->toString());
